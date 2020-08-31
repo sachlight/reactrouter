3449c02e472fc63f03931e302650f55640f1b69d
drishty <drishty.9brainz@gmail.com>
2020-08-31 17:20:59 +0530

    Updates

---
+++ precache-manifest.29141694a4612b4f51fd5f6b26776fb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fa4cf1446928e7c69f4",
+    "url": "/reactrouter/static/css/main.3c94da41.chunk.css"
+  },
+  {
+    "revision": "6fa4cf1446928e7c69f4",
+    "url": "/reactrouter/static/js/main.6fa4cf14.chunk.js"
+  },
+  {
+    "revision": "5ed65607439115468c5a",
+    "url": "/reactrouter/static/js/1.5ed65607.chunk.js"
+  },
+  {
+    "revision": "61028d729e55d2e47fae",
+    "url": "/reactrouter/static/js/runtime~main.61028d72.js"
+  },
+  {
+    "revision": "d6da2b20e400b169dfc64acd96155bb6",
+    "url": "/reactrouter/index.html"
+  }
+];
